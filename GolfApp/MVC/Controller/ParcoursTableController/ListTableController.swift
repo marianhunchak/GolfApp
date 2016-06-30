fb138bd08ebeda848c0d613195a35a1fd02d94df
demkovskyi <volodimir.demkovsky@yandex.ru>
2016-07-01 01:48:14 +0300

    changed images in course 1

--- GolfApp/MVC/Controller/ParcoursTableController/ListTableController.swift
+++ GolfApp/MVC/Controller/ParcoursTableController/ListTableController.swift
@@ -12,12 +12,12 @@ private let reuseIdentifier = "parcoursCell"
 private let parcouseTableCellNibname = "CoursTableCell"
 private let detailConreoolerIdentfier = "DetailViewController"
 
-private let course_1_Images = ["course_1_hole_1.jpg",  "course_1_hole_2.jpg",  "course_1_hole_3.jpg",
-                               "course_1_hole_4",      "course_1_hole_5.jpg",  "course_1_hole_6.jpg",
-                               "course_1_hole_7.jpg",  "course_1_hole_8.jpg",  "course_1_hole_9.jpg",
-                               "course_1_hole_10.jpg", "course_1_hole_11.jpg", "course_1_hole_12.jpg",
-                               "course_1_hole_13.jpg", "course_1_hole_14.jpg", "course_1_hole_15.jpg",
-                               "course_1_hole_16.jpg", "course_1_hole_17.jpg", "course_1_hole_18.jpg"]
+private let course_1_Images = ["course_1_hole_01.jpg",  "course_1_hole_02.jpg",  "course_1_hole_03.jpg",
+                               "course_1_hole_04.jpg",  "course_1_hole_05.jpg",  "course_1_hole_06.jpg",
+                               "course_1_hole_07.jpg",  "course_1_hole_08.jpg",  "course_1_hole_09.jpg",
+                               "course_1_hole_10.jpg",  "course_1_hole_11.jpg",  "course_1_hole_12.jpg",
+                               "course_1_hole_13.jpg",  "course_1_hole_14.jpg",  "course_1_hole_15.jpg",
+                               "course_1_hole_16.jpg",  "course_1_hole_17.jpg",  "course_1_hole_18.jpg"]
 
 private let course_2_Images = ["course_2_hole_1.jpg", "course_2_hole_2.jpg", "course_2_hole_3.jpg",
                                "course_2_hole_4",     "course_2_hole_5.jpg", "course_2_hole_6.jpg",
